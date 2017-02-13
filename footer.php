b48ee73d931bf295abe41bcb2c5fdf006c8ffb92
John Beales <john@johnbeales.com>
2017-02-13 14:16:45 -0500

    Update footer link to johnbeales.com

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_jb' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', '_jb' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', '_jb' ), '_jb', '<a href="https://automattic.com/" rel="designer">Automattic</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', '_jb' ), '_jb', '<a href="http://johnbeales.com/" rel="designer">John Beales</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
