275bf9adcdeb30815c5109ff3a87858c1c9094b1
John Beales <john@johnbeales.com>
2019-03-20 15:08:30 -0400

    Add styles for basic excerpts. Add "Follow on Twitter" prompt on Single pages. Add more social icons for the future.

---
+++ template-parts/social-prompt.php
@@ -0,0 +1,5 @@
+<div class="social-prompt">
+	<p class="follow-me"><?php
+		echo file_get_contents(get_theme_file_path('img/icons/twitter.svg'));
+	?>Follow me <a href="https://www.twitter.com/johnbeales">@johnbeales</a></p>
+</div>
