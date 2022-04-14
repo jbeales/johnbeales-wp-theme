599d830a9844923b2e6adebfeab0236163350081
John Beales <john@johnbeales.com>
2022-04-14 20:40:08 +0000

    Enqueue typekit fonts.

--- functions.php
+++ functions.php
@@ -101,6 +101,8 @@ add_action( 'widgets_init', 'johnbeales_widgets_init' );
  * Enqueue scripts and styles.
  */
 function johnbeales_scripts() {
+	wp_enqueue_style( 'johnbeales-adobe-fonts', 'https://use.typekit.net/uta8oqn.css' );
+	
 	wp_enqueue_style( 'johnbeales-style', get_stylesheet_uri(), array(), johnbeales_theme_version() );
 
 	wp_enqueue_script( 'johnbeales-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
